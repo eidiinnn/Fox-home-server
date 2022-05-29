2247790523eed02ac5108de61943094c5b109f35
Eduardo da Silva <eduardosilva.pn@gmail.com>
2022-05-29 18:16:21 +0000

    tests for login  validations

---
+++ __tests__/account/test-verifyLogin.ts
@@ -0,0 +1,23 @@
+import Account from '../../src/accountSystem/account';
+import MongoDB from '../../src/database/mongoDB';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const accountFunctions = new Account('AccountTest');
+
+beforeAll(async () => {
+  const db = new MongoDB(process.env.MONGODB_URI as string);
+  db.connect();
+  await accountFunctions.dbModel.remove({});
+});
+
+test('verify password works', async () => {
+  const accountCreated = await accountFunctions.create({
+    email: 'eduardo@testVerifyPassword.com',
+    password: 'testPassNewUser',
+  });
+
+  return expect(
+    accountFunctions.verifyPassword(accountCreated._id, 'testPassNewUser')
+  ).toBe(true);
+});
